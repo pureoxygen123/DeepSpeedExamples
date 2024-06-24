8388d1ce9acbbf9ee5dc01a7ff67b2a408e6e2f9
YoungChen0321 <53079390+pureoxygen123@users.noreply.github.com>
2024-06-24 15:48:25 +0800

    Delete applications/DeepSpeed-Chat/dschat/utils/data/test.py

--- applications/DeepSpeed-Chat/dschat/utils/data/test.py
+++
@@ -1,7 +0,0 @@
-from raw_datasets import DahoasFullhhrlhfDataset
-
-dataset = DahoasFullhhrlhfDataset()
-train_data = dataset.get_train_data()
-eval_data = dataset.get_eval_data()
-print(train_data)
-print(eval_data)
