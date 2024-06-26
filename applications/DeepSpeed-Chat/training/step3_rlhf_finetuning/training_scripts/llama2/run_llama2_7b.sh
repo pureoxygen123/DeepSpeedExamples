a1302b3ab691afed0e3f99763a7cb4e9c166d3c8
YoungChen0321 <53079390+pureoxygen123@users.noreply.github.com>
2024-06-26 14:37:25 +0800

    Update run_llama2_7b.sh

--- applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/llama2/run_llama2_7b.sh
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/llama2/run_llama2_7b.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 # Copyright (c) Microsoft Corporation.
 # SPDX-License-Identifier: Apache-2.0
-export PATH="/fs-computility/llm/shared/rl3m_env/dep/cuda-11.7/bin:$PATH"
-export LD_LIBRARY_PATH="/fs-computility/llm/shared/rl3m_env/dep/cuda-11.7/lib64":$LD_LIBR
-export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LIBRARY_PATH
-export C_INCLUDE_PATH=/usr/lib/x86_64-linux-gnu:$C_INCLUDE_PATH
-export TORCH_CUDNN_V8_API_DISABLED=1
 # DeepSpeed Team
-ACTOR_MODEL_PATH="/fs-computility/llm/chenyang2/OpenLLMAI/Llama-2-7b-sft-model-ocra-500k"
-CRITIC_MODEL_PATH="/fs-computility/llm/chenyang2/hf_model/OpenLLMAI/Llama-2-7b-rm-anthropic_hh-lmsys-oasst-webgpt/models--OpenLLMAI--Llama-2-7b-rm-anthropic_hh-lmsys-oasst-webgpt/snapshots/a982afeed00fac9767d53aecde5b88947b1be194"
+ACTOR_MODEL_PATH="OpenLLMAI/Llama-2-7b-sft-model-ocra-500k"
+CRITIC_MODEL_PATH="OpenLLMAI/Llama-2-7b-rm-anthropic_hh-lmsys-oasst-webgpt/models--OpenLLMAI--Llama-2-7b-rm-anthropic_hh-lmsys-oasst-webgpt"
 ACTOR_ZERO_STAGE=3
 CRITIC_ZERO_STAGE=3
 OUTPUT=$5
@@ -29,17 +24,17 @@ Actor_Lr=9.65e-6
 Critic_Lr=5e-6
 
 deepspeed --num_nodes=4 \
-   -H /fs-computility/llm/chenyang2/DeepSpeedExamples-master/applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/training_scripts/llama2/hostfile \
+   -H hostfile1 \
     main.py \
    --data_path Dahoas/full-hh-rlhf \
    --data_split 2,4,4 \
    --actor_model_name_or_path $ACTOR_MODEL_PATH \
    --critic_model_name_or_path $CRITIC_MODEL_PATH \
    --num_padding_at_beginning 1 \
-   --per_device_generation_batch_size 1 \
-   --per_device_training_batch_size 1 \
-   --generation_batches 1 \
-   --gradient_accumulation_steps 1 \
+   --per_device_generation_batch_size 8 \
+   --per_device_training_batch_size 8 \
+   --generation_batches 4 \
+   --gradient_accumulation_steps 4 \
    --ppo_epochs 1 \
    --max_answer_seq_len 1024 \
    --max_prompt_seq_len 1024 \
@@ -58,9 +53,7 @@ deepspeed --num_nodes=4 \
    --critic_zero_stage $CRITIC_ZERO_STAGE \
    --enable_hybrid_engine \
    --output_dir $OUTPUT \
-    &> $OUTPUT/training-test-fordata.log
+    &> $OUTPUT/training.log
 
 #    --offload \
 #    --offload_reference_model \
-#    --dtype bf16 \
-#    --unpin_actor_parameters \
